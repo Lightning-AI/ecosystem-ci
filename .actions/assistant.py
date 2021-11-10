db757f02018783fb99d2e1fd7728cf56dbd77029
Jirka <jirka.borovec@seznam.cz>
2021-11-10 11:25:30 +0100

    multi OS

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -9,7 +9,7 @@ _PATH_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 class AssistantCLI:
 
-    _BASH_SCRIPT = ("#!/bin/bash", "set -e")
+    _BASH_SCRIPT = ("set -e", )
     _FIELD_TARGET_REPO = "target_repository"
     _FIELD_REQ = "dependencies"
     _FIELD_TESTS = "testing"
@@ -87,13 +87,14 @@ class AssistantCLI:
             if isinstance(repo["copy_tests"], str):
                 repo["copy_tests"] = [repo["copy_tests"]]
             for test in repo["copy_tests"]:
+                test = test.replace("/", os.path.sep)
                 path_test = os.path.join(path_root, repo_name, test)
                 test_dir = os.path.dirname(test)
                 if test_dir:
-                    script.append(f"mkdir -p {test_dir}")
+                    script.append(f'mkdir -p "{test_dir}"')
                 is_file = os.path.splitext(test)[-1] != ""
-                script.append(f"cp {'-r' if not is_file else ''} {path_test} {test}")
-        script.append(f"rm -rf {repo_name}")
+                script.append(f'cp {"-r" if not is_file else ""} "{path_test}" "{test}"')
+        script.append(f'rm -rf "{repo_name}"')
 
         reqs = config.get("dependencies", [])
         for req in reqs:
