b15b871a4a80e8cb6e136337f0a98befd08a4a05
Jirka <jirka.borovec@seznam.cz>
2021-12-15 10:52:06 +0100

    req

--- actions/test_cli.py
+++ actions/test_cli.py
@@ -9,7 +9,17 @@ _PATH_CONFIG = os.path.join(_PATH_DIR, "_config.yaml")
 
 
 @pytest.mark.parametrize(
-    "cmd", ["folder_repo", "contacts", "list_env", "before_commands", "prepare_env", "specify_tests"]
+    "cmd",
+    [
+        "folder_repo",
+        "find_all_configs",
+        "list_runtimes",
+        "contacts",
+        "list_env",
+        "before_commands",
+        "prepare_env",
+        "specify_tests",
+    ],
 )
 def test_assistant_commands(cmd):
     AssistantCLI().__getattribute__(cmd)(_PATH_CONFIG)
