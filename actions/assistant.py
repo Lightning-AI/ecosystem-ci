ca9f573bcd0116ccd802e8ad111e3176770ebc3d
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-10-19 10:42:55 +0200

    set depth to 100

--- actions/assistant.py
+++ actions/assistant.py
@@ -168,7 +168,7 @@ class AssistantCLI:
         url = AssistantCLI._https(
             repo.get("HTTPS"), token=repo.get("token"), username=repo.get("username"), password=repo.get("password")
         )
-        cmd_git = f"git clone {url} --depth 0"
+        cmd_git = f"git clone {url} --depth 100"
         repo_name, _ = os.path.splitext(os.path.basename(repo.get("HTTPS")))
         cmds = [cmd_git, f"cd {repo_name}"]
         if "checkout" in repo:
